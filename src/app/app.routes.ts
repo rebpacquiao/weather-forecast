7eacf94776b426714f33d879747e6d6e2587b46b
rebpacquiao <reb.pacquiao@gmail.com>
2024-04-12 14:41:45 +0800

    Refactor routing configuration in app.module.ts and app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { WeatherComponent } from './weather/weather.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirect to login page by default
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'weather', component: WeatherComponent },
+];
